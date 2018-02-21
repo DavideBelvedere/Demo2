4844706e7a3c7a9b44e7a2e66b6852dfdd66ef41
DavideBelvedere <davide.belvedere@gmail.com>
2018-02-21 11:17:57 +0100

    creato l'header

--- src/app/character.ts
+++ src/app/character.ts
@@ -2,10 +2,10 @@ export class Character {
     type: string;
     name: string;
     age: number;
-    constructor(type: string="", name: string="", age: number = 0) {
+    constructor(type: string = "", name: string = "", age: number = 0) {
         this.type = type;
         this.name = name;
         this.age = age;
     }
-    
+
 }
